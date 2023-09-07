232800ed6da3ad8718c0c7b18a3498ca126d9a45
ilyakrivonozhko <ilya.krivonozhko@gmail.com>
2023-09-07 15:34:17 +0800

    Project initialization.

---
+++ config/routes.rb
@@ -0,0 +1,11 @@
+Rails.application.routes.draw do
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # namespace :api do
+  #   namespace :v1 do
+  #     resources :books
+  #   end
+  # end
+  # root "articles#index"
+end
